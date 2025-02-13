ee29f71666293ee6940285cfe34400047042ccce
amr248-ua <amr248@gcloud.ua.es>
2025-02-13 20:01:41 +0100

    Eliminado controller

--- src/main/java/com/miproyecto/gamestack/GameStackApplication.java
+++ src/main/java/com/miproyecto/gamestack/GameStackApplication.java
@@ -2,21 +2,13 @@ package com.miproyecto.gamestack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
-@RestController
 public class GameStackApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GameStackApplication.class, args);
-	}
-
-	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
 	}
 
 }
