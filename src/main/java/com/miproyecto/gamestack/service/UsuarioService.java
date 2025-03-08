cb28e806da9f82349faa63134c9dd5dbd9c5eebd
amr248-ua <amr248@gcloud.ua.es>
2025-03-08 12:25:25 +0100

    Servicio para registrar usuarios

---
+++ src/main/java/com/miproyecto/gamestack/service/UsuarioService.java
@@ -0,0 +1,35 @@
+package com.miproyecto.gamestack.service;
+
+import com.miproyecto.gamestack.dto.UsuarioData;
+import com.miproyecto.gamestack.model.Usuario;
+import com.miproyecto.gamestack.repository.UsuarioRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+public class UsuarioService {
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @Transactional
+    public UsuarioData registrar(UsuarioData usuario) {
+        Optional<Usuario> usuarioBD = usuarioRepository.findByEmail(usuario.getEmail());
+        if (usuarioBD.isPresent())
+            throw new UsuarioServiceException("El usuario " + usuario.getEmail() + " ya está registrado");
+        else if (usuario.getEmail() == null)
+            throw new UsuarioServiceException("El usuario no tiene email");
+        else if (usuario.getPassword() == null)
+            throw new UsuarioServiceException("El usuario no tiene password");
+        else {
+            Usuario usuarioNuevo = modelMapper.map(usuario, Usuario.class);
+            usuarioNuevo = usuarioRepository.save(usuarioNuevo);
+            return modelMapper.map(usuarioNuevo, UsuarioData.class);
+        }
+    }
+}
