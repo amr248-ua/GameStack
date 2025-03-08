8130206aa9558c41e061452eb79f0b0ccfca0169
amr248-ua <amr248@gcloud.ua.es>
2025-03-08 12:24:49 +0100

    Repository para crud de usuario

---
+++ src/main/java/com/miproyecto/gamestack/repository/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package com.miproyecto.gamestack.repository;
+
+import com.miproyecto.gamestack.model.Usuario;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends CrudRepository<Usuario, Long> {
+    Optional<Usuario> findByEmail(String s);
+}
