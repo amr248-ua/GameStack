44efb46d81cef59f80c8f045d4c3869762316247
amr248-ua <amr248@gcloud.ua.es>
2025-03-11 13:01:35 +0100

    Añadidas validaciones para el registro de un usuario

--- src/main/java/com/miproyecto/gamestack/dto/RegistroData.java
+++ src/main/java/com/miproyecto/gamestack/dto/RegistroData.java
@@ -1,6 +1,8 @@
 package com.miproyecto.gamestack.dto;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,10 +11,13 @@ import java.time.LocalDate;
 @Getter
 @Setter
 public class RegistroData {
-    @Email
+    @Email(message = "El email no es válido")
     private String email;
+    @Size(min = 6, message = "La contraseña debe tener al menos 6 caracteres")
     private String password;
+    @Size(min = 3, max = 20, message = "El nombre de usuario debe tener entre 3 y 20 caracteres")
     private String username;
+    @Past(message = "La fecha de nacimiento debe ser en el pasado")
     private LocalDate fechaNacimiento;
 
 }
