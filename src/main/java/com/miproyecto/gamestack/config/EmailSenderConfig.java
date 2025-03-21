763dec424a2009c4df1c5cbcd2514d67597d2e03
amr248-ua <amr248@gcloud.ua.es>
2025-03-21 18:58:54 +0100

    Corregida configuracion para inyectar correctamente la dependencia de javamailsender

---
+++ src/main/java/com/miproyecto/gamestack/config/EmailSenderConfig.java
@@ -0,0 +1,29 @@
+package com.miproyecto.gamestack.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class EmailSenderConfig {
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+        mailSender.setUsername("amr248@gcloud.ua.es");
+        mailSender.setPassword("ryqd sccj giut mbrk");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.starttls.required", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+}
